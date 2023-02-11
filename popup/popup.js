85dbf4c2d52100511a70e971f8978d9eca2ad802
Dev-Srivastava <devsrivastava898@gmail.com>
2023-02-11 22:37:31 +0530

    Extension update

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,27 @@
-fetch('https://api.websitecarbon.com/v1/?url=' + window.location.href)
-  .then(response => response.json())
-  .then(data => {
+async function getWebsiteData() {
+  const getCurrentTab = () => new Promise(resolve => {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      resolve(tabs[0].url);
+    });
+  });
+
+  const url = await getCurrentTab();
+  if (!url) {
+    document.querySelector('#bytes').innerHTML = 'Error';
+    document.querySelector('#carbon').innerHTML = 'Error';
+    return;
+  }
+  const apiEndpoint = `https://api.websitecarbon.com/v1/?url=${encodeURIComponent(url)}`;
+  try {
+    const response = await fetch(apiEndpoint);
+    const data = await response.json();
     document.querySelector('#bytes').innerHTML = data.bytes + ' B';
     document.querySelector('#carbon').innerHTML = data.carbon + ' gCO2';
-  });
+  } catch (error) {
+    console.error(error);
+    document.querySelector('#bytes').innerHTML = 'Error';
+    document.querySelector('#carbon').innerHTML = 'Error';
+  }
+}
+
+getWebsiteData();
